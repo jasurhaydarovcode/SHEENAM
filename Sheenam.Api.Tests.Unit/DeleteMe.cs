152f11764a58218839613a4ad812d62f1b78b5f8
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2025-03-29 13:31:38 +0500

    INFRA: Initialize Build Project

--- Sheenam.Api.Tests.Unit/DeleteMe.cs
+++ Sheenam.Api.Tests.Unit/DeleteMe.cs
@@ -8,6 +8,6 @@ namespace Sheenam.Api.Tests.Unit
     public class DeleteMe
     {
         [Fact]
-        public void ShouldBeTrue() => Assert.True(true);
+        public void ShouldBeSucesful() => Assert.True(true);
     }
 }
